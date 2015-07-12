b500f3ae66afaf1eca0851d61f5c948ce494b4f9
Diksha <diksha.2895@gmail.com>
2015-07-12 21:16:46 +0530

    updated wsgi to cling

--- find-my-place/find-my-place/wsgi.py
+++ find-my-place/find-my-place/wsgi.py
@@ -14,12 +14,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "find-my-place.settings")
 
-from whitenoise.django import DjangoWhiteNoise
+#from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+#application = DjangoWhiteNoise(application)
 
-"""from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-application = Cling(get_wsgi_application())"""
+application = Cling(get_wsgi_application())
