ab594bff690b5065d204f430c97ca03d9c190a17
Diksha <diksha.2895@gmail.com>
2015-07-12 21:01:27 +0530

    updated wasgi with django_setting_module before django whitenose

--- find-my-place/find-my-place/wsgi.py
+++ find-my-place/find-my-place/wsgi.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "find-my-place.settings")
+
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
