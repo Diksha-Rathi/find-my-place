05f88721e68478b61d1976219cbfac45faae81bc
Diksha <diksha.2895@gmail.com>
2015-07-12 19:48:51 +0530

    updated src in templates

--- find-my-place/find-my-place/wsgi.py
+++ find-my-place/find-my-place/wsgi.py
@@ -20,3 +20,8 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(get_wsgi_application())
